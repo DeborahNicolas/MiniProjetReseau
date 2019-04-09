aceda69d94944dc3716df5252f2250e9dc3a56dd
dnicolas <dnicolas@cst-tp-b10617>
2019-04-09 17:54:23 +0200

    Le serveur génère une clé partagée, il la chiffre avec la clé publique reçue

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,10 +1,14 @@
 
+import com.sun.org.apache.xml.internal.security.utils.Base64;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 public class ChatServer implements Runnable{
 	PrintWriter out;
@@ -22,12 +26,35 @@ public class ChatServer implements Runnable{
 	}
 	
 	public void run(){
+            
+            
 		try{
+                    
+                        
 			in = new BufferedReader(new InputStreamReader(s.getInputStream()));
 			out = new PrintWriter(s.getOutputStream());
 			
 			System.out.println("connexion de "+s.getInetAddress().toString()+" sur le port "+s.getPort());
 			String talk = in.readLine();
+                        
+                        
+                        //On génère la clé partagée
+                        
+                        KeyGenerator generator = KeyGenerator.getInstance("AES");
+                        generator.init(128);
+                        SecretKey key = generator.generateKey();
+                        
+                        System.out.println("cle partagee : " + key);
+                        
+                        //On chiffre la clé partagée 
+                        
+                        Cipher cipher = Cipher.getInstance("AES");
+                        cipher.init(Cipher.ENCRYPT_MODE, key);
+                        byte[] res = cipher.doFinal(talk.getBytes());
+                        String res_str =  Base64.encode(res);//new String(res);
+                        
+                        System.out.println("cle partagee codee : " + res_str);
+                        
 			while(doRun)
 			{
 				while(talk == null)
